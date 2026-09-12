ba1a6ff3a2f1ab0477863bc16e7de7692eff9258
Álvaro Gordillo <alvaro.gordillo.reina@gmail.com>
2026-09-12 16:49:08 +0200

    session on render deploy web site

--- app/sessions.server.ts
+++ app/sessions.server.ts
@@ -17,7 +17,7 @@ const { commitSession, destroySession, getSession } =
       name: "__session",
 
       // all of these are optional
-      domain: "localhost",
+      domain: "demo-react-router-framework-1.onrender.com",
       // Expires can also be set (although maxAge overrides it when used in combination).
       // Note that this method is NOT recommended as `new Date` creates only one date on each server deployment, not a dynamic date in the future!
       //
